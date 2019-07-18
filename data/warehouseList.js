1f4efafafbd01df8b80cedbe142483c34eac5c09
Mario Delgado <51416273+MarioDelgadoSr@users.noreply.github.com>
2019-07-18 19:31:29 -0400

    Reworked for Warehouse

---
+++ data/warehouseList.js
@@ -0,0 +1,1 @@
+var warehouseList = ["Small","Medium","Large"];
---
+++ translations/Development/translations2.js
@@ -0,0 +1,236 @@
+﻿// Example: translate('datGuiNoColor');
+var translationsNew = {
+
+	"es":		{
+		"appTitle":					"Visualizador de datos",
+		"datGuiOriginalColor":					"Original",
+		"dataGridTitle":					"Datos",
+		"helpText3":					"<br> CtrlKey + Izquierda: seleccione un objeto y muestre los detalles en el área de Controles <br> <br> <b> Teclas </b>: <br> <br> Flechas: Paneo 'b': 'B' ird's-eye View / Rotate <br> 'c' o 'Esc': 'C'lear Data Selections <br>' d ':' D'ata Grid <br> 'h': 'H'ide / Re-display Menú",
+		"datGuiFolders.selection.folders.data":					"Datos",
+		"datGuiFolders.dataGrid":					"Cuadrícula de datos"
+		},//"es"
+	"sv":		{
+		"appTitle":					"Data Visualizer",
+		"datGuiOriginalColor":					"Original",
+		"dataGridTitle":					"Data",
+		"helpText3":					"<br> CtrlKey + Vänster: Välj ett objekt och visa detaljer i kontrollområdet <br> <br> <b> Tangenter </ b>: <br> <br> Pilar: Pan <br> 'b': 'B' ird's-eye Visa / Rotera <br> 'c' eller 'Esc': 'C'lear Data Selections <br>' d ':' D'ata Grid <br> 'h': 'H'ide / Re-display Meny",
+		"datGuiFolders.selection.folders.data":					"Data",
+		"datGuiFolders.dataGrid":					"Datanätet"
+		},//"sv"
+	"fi":		{
+		"appTitle":					"Data Visualizer",
+		"datGuiOriginalColor":					"Alkuperäinen",
+		"dataGridTitle":					"data",
+		"helpText3":					"<br> CtrlKey + Vasen: Valitse kohde ja näyttötiedot kohdasta Ohjausalue <br> <br> <b> Avaimet </b>: <br> <br> Nuolet: Pan <br> 'b': 'B' ird's-eye Näytä / Kierrä 'c' tai 'Esc': 'C'lear Data Selections <br>' d ':' D'ata Grid <br> 'h': 'H'ide / Re-display valikko",
+		"datGuiFolders.selection.folders.data":					"data",
+		"datGuiFolders.dataGrid":					"Data Grid"
+		},//"fi"
+	"fr":		{
+		"appTitle":					"Visualiseur de données",
+		"datGuiOriginalColor":					"Original",
+		"dataGridTitle":					"Les données",
+		"helpText3":					"<br> CtrlTouche + Gauche: sélection d'un objet et affichage des détails dans la zone de contrôle <br> <br> <b> Touches </ b>: <br> <br> Flèches: panoramique <br> 'b': 'B' ird's-eye View / Rotation <br> 'c' ou 'Esc': 'C'lear Data Selections <br>' d ':' D'ata Grid <br> 'h': 'H'ide / Re-display Menu",
+		"datGuiFolders.selection.folders.data":					"Les données",
+		"datGuiFolders.dataGrid":					"Grille de données"
+		},//"fr"
+	"da":		{
+		"appTitle":					"Data Visualizer",
+		"datGuiOriginalColor":					"Original",
+		"dataGridTitle":					"Data",
+		"helpText3":					"<br> CtrlKey + Venstre: Vælg et objekt og vis detaljer i Kontrol område <br> <br> <b> Taster </ b>: <br> <br> Pile: Pan <br> 'b': 'B' ird's-eye View / Rotate <br> 'c' eller 'Esc': 'C'lear Data Selections <br>' d ':' D'ata Grid <br> 'h': 'H'ide / Re-display Menu",
+		"datGuiFolders.selection.folders.data":					"Data",
+		"datGuiFolders.dataGrid":					"Data Gitter"
+		},//"da"
+	"lt":		{
+		"appTitle":					"Duomenų vizualizatorius",
+		"datGuiOriginalColor":					"Originalas",
+		"dataGridTitle":					"Duomenys",
+		"helpText3":					"<br> CtrlKey + Left: pasirinkite objektą ir išsamią informaciją „Controls“ srityje <br> <br> <b> Raktai </b>: <br> <br> Rodyklės: Pan <br> „b“: „B“ ird's-eye Peržiūrėti / pasukti 'c' arba 'Esc': 'C'lear Data Selections <br>' d ':' D'ata Grid <br> 'h': 'H'ide / Re-display Meniu",
+		"datGuiFolders.selection.folders.data":					"Duomenys",
+		"datGuiFolders.dataGrid":					"Duomenų tinklelis"
+		},//"lt"
+	"pl":		{
+		"appTitle":					"Wizualizator danych",
+		"datGuiOriginalColor":					"Oryginalny",
+		"dataGridTitle":					"Dane",
+		"helpText3":					"<br> CtrlKey + Lewo: Wybierz obiekt i wyświetl szczegóły w obszarze Kontrolki <br> <br> <b> Klawisze </b>: <br> <br> Strzałki: Pan <br> „b”: „B” ird's-eye Wyświetl / Obróć <br> „c” lub „Esc”: „Wybór danych C'lear <br>„ d ”:„ Siatka D'ata ”„ h ”:„ H'ide / Re-display Menu",
+		"datGuiFolders.selection.folders.data":					"Dane",
+		"datGuiFolders.dataGrid":					"Siatka danych"
+		},//"pl"
+	"id":		{
+		"appTitle":					"Visualizer Data",
+		"datGuiOriginalColor":					"Asli",
+		"dataGridTitle":					"Data",
+		"helpText3":					"<br> CtrlKey + Kiri: Pilih objek dan tampilkan detail di area Kontrol <br> <br> <br> <b> Tombol </b>: <br> <br> Panah: Pan <br> 'b': 'B' ird's-eye Lihat / Putar <br> 'c' atau 'Esc': 'Seleksi Data C'lear <br>' d ':' D'ata Grid <br> 'h': 'Sembunyikan / Tampilan ulang Menu",
+		"datGuiFolders.selection.folders.data":					"Data",
+		"datGuiFolders.dataGrid":					"Kisi Data"
+		},//"id"
+	"fil":		{
+		"appTitle":					"Data Visualizer",
+		"datGuiOriginalColor":					"Orihinal",
+		"dataGridTitle":					"Data",
+		"helpText3":					"<br> CtrlKey + Kaliwa: Pumili ng isang bagay at mga detalye ng display sa Mga kontrol na lugar <br> <br> <b> Mga Key </ b>: <br> <br> Mga arrow: Pan <br> 'b': 'B' Tingnan ang / i-rotate ang 'c' o 'Esc': 'C'lear Data Selections sa' d ':' D'ata Grid sa 'h': 'H'ide / Re-display Menu",
+		"datGuiFolders.selection.folders.data":					"Data",
+		"datGuiFolders.dataGrid":					"Data Grid"
+		},//"fil"
+	"zh-CN":		{
+		"appTitle":					"数据显示器",
+		"datGuiOriginalColor":					"原版的",
+		"dataGridTitle":					"数据",
+		"helpText3":					"<br> CtrlKey + Left：选择一个对象并在控件区域中显示详细信息<br> <br> <b>键</ b>：<br> <br>箭头：Pan <br>'b'：'B' ird-eye View / Rotate <br>'c'或'Esc'：'C'lear Data Selections <br>'d'：'D'ata Grid <br>'h'：'H'ide / Re-display菜单",
+		"datGuiFolders.selection.folders.data":					"数据",
+		"datGuiFolders.dataGrid":					"数据网格"
+		},//"zh-CN"
+	"cs":		{
+		"appTitle":					"Data Visualizer",
+		"datGuiOriginalColor":					"Originál",
+		"dataGridTitle":					"Data",
+		"helpText3":					"<br> CtrlKey + Left: Vyberte objekt a zobrazte podrobnosti v oblasti Controls <br> <br> <b> Klíče </b>: <br> <br> Šipky: Pan <br> 'b': 'B' ird's-eye Zobrazit / Otočit <br> 'c' nebo 'Esc': 'C'lear Data Selections' 'd': 'D'ata Grid <br>' h ':' H'ide / Re-display Jídelní lístek",
+		"datGuiFolders.selection.folders.data":					"Data",
+		"datGuiFolders.dataGrid":					"Datová mřížka"
+		},//"cs"
+	"nl":		{
+		"appTitle":					"Data Visualizer",
+		"datGuiOriginalColor":					"origineel",
+		"dataGridTitle":					"Gegevens",
+		"helpText3":					"<br> CtrlKey + Left: Selecteer een object en geef details weer in het gedeelte Controls <br> <br> <b> Keys </ b>: <br> <br> Arrows: Pan <br> 'b': 'B' ird's-eye Bekijken / Roteren <br> 'c' of 'Esc': 'C'lear Gegevensselecties <br>' d ':' D'ata Grid <br> 'h': 'H'ide / Re-display Menu",
+		"datGuiFolders.selection.folders.data":					"Gegevens",
+		"datGuiFolders.dataGrid":					"Data rooster"
+		},//"nl"
+	"et":		{
+		"appTitle":					"Andmete visualiseerija",
+		"datGuiOriginalColor":					"Originaal",
+		"dataGridTitle":					"Andmed",
+		"helpText3":					"<br> CtrlKey + vasak: vali objekt ja kuvada üksikasjad Controls'i alal <br> <br> <b> Klahvid </b>: <br> <br> Nooled: Pan <br> 'b': 'B' ird's-eye Vaata / Pööra 'c' või 'Esc': 'C'lear Data Selections <br>' d ':' D'ata Grid <br> 'h': 'H'ide / Re-display Menüü",
+		"datGuiFolders.selection.folders.data":					"Andmed",
+		"datGuiFolders.dataGrid":					"Andmevõrk"
+		},//"et"
+	"de":		{
+		"appTitle":					"Datenvisualisierer",
+		"datGuiOriginalColor":					"Original",
+		"dataGridTitle":					"Daten",
+		"helpText3":					"<br> CtrlKey + Left: Wählen Sie ein Objekt aus und zeigen Sie Details im Bereich Controls an. <br> <br> <b> Tasten </ b>: <br> <br> Pfeile: Pan <br> 'b': 'B' ird-eye-Ansicht / Drehen <br> 'c' oder 'Esc': 'C'lear-Datenauswahl <br>' d ':' D'ata-Raster <br> 'h': 'H'ide / Re-display Speisekarte",
+		"datGuiFolders.selection.folders.data":					"Daten",
+		"datGuiFolders.dataGrid":					"Datenraster"
+		},//"de"
+	"el":		{
+		"appTitle":					"Έλεγχος δεδομένων",
+		"datGuiOriginalColor":					"Πρωτότυπο",
+		"dataGridTitle":					"Δεδομένα",
+		"helpText3":					"<br> CtrlKey + Αριστερά: Επιλέξτε ένα αντικείμενο και να εμφανίσετε λεπτομέρειες στην περιοχή ελέγχου <br> <br> <b> Πλήκτρα </ b>: <br> <br> Βέλη: Pan <br> 'b': 'B' Προβολή / Περιστροφή <br> 'c' ή 'Esc': 'Επιλογές Δεδομένων C'lear <br>' d ':' D'ata Πλέγμα <br> 'h': 'H'ide / Re-display Μενού",
+		"datGuiFolders.selection.folders.data":					"Δεδομένα",
+		"datGuiFolders.dataGrid":					"Πλέγμα δεδομένων"
+		},//"el"
+	"he":		{
+		"appTitle":					"נתונים Visualizer",
+		"datGuiOriginalColor":					"מקורי",
+		"dataGridTitle":					"נתונים",
+		"helpText3":					"<br> CtrlKey + Left: בחירת אובייקט ופרטי תצוגה באזור Controls <br> <br> <b> מקשים </ b> צפייה בעין / סיבוב - 'c' או 'Esc': 'Clele Data Selections' 'd': 'ד' ata Grid <br> 'h': 'H'ide / Re-display תפריט",
+		"datGuiFolders.selection.folders.data":					"נתונים",
+		"datGuiFolders.dataGrid":					"רשת נתונים"
+		},//"he"
+	"is":		{
+		"appTitle":					"Data Visualizer",
+		"datGuiOriginalColor":					"Upprunalega",
+		"dataGridTitle":					"Gögn",
+		"helpText3":					"<br> CtrlKey + Vinstri: Veldu hlut og sýna upplýsingar í Stjórntökusvæði <br> <br> <b> Lyklar </ b>: <br> <br> Örvar: Pan <br> 'b': 'B' Skyggnusýning / Snúa <br> 'c' eða 'Esc': 'Gagnaskiptaval' <br> 'd': 'Skrímsli Grid <br>' h ':' H'ide / Re-sýna Valmynd",
+		"datGuiFolders.selection.folders.data":					"Gögn",
+		"datGuiFolders.dataGrid":					"Gögn Grid"
+		},//"is"
+	"it":		{
+		"appTitle":					"Data Visualizer",
+		"datGuiOriginalColor":					"Originale",
+		"dataGridTitle":					"Dati",
+		"helpText3":					"<br> CtrlKey + Sinistra: seleziona un oggetto e visualizza i dettagli nell'area Controls <br> <br> <b> Chiavi </ b>: <br> <br> Frecce: Pan <br> 'b': 'B' Vista ird's-eye / Rotazione <br> 'c' o 'Esc': 'C'lear Data Selections <br>' d ':' D'ata Grid <br> 'h': 'H'ide / Re-display Menu",
+		"datGuiFolders.selection.folders.data":					"Dati",
+		"datGuiFolders.dataGrid":					"Data Grid"
+		},//"it"
+	"ja":		{
+		"appTitle":					"データビジュアライザ",
+		"datGuiOriginalColor":					"元の",
+		"dataGridTitle":					"データ",
+		"helpText3":					"<br> Ctrlキー+左：オブジェクトを選択してコントロール領域に詳細を表示します。<br> <br> <b>キー</b>：<br> <br>矢印：Pan <br> 'b'： 'B' 'c'または 'Esc'： 'C'クリアデータ選択<br> 'd'： 'D'グリッド 'h'： '非表示/再表示メニュー",
+		"datGuiFolders.selection.folders.data":					"データ",
+		"datGuiFolders.dataGrid":					"データグリッド"
+		},//"ja"
+	"ko":		{
+		"appTitle":					"데이터 시각화 장치",
+		"datGuiOriginalColor":					"기발한",
+		"dataGridTitle":					"데이터",
+		"helpText3":					"<br> Ctrl 키 + 왼쪽 : 컨트롤 영역에 개체를 선택하고 세부 정보를 표시 <br> <br> <b> 키 </ b> : <br> <br> 화살표 : 팬 - 'b': 'B' ird's-eye 'c'또는 'Esc'보기 / 회전 : 'C'lear 데이터 선택 <br>'d ':'D 'at Grid'h ':'H'ide / Re-display 메뉴",
+		"datGuiFolders.selection.folders.data":					"데이터",
+		"datGuiFolders.dataGrid":					"데이터 그리드"
+		},//"ko"
+	"lv":		{
+		"appTitle":					"Datu vizualizators",
+		"datGuiOriginalColor":					"Oriģināls",
+		"dataGridTitle":					"Dati",
+		"helpText3":					"<br> CtrlKey + pa kreisi: atlasiet objektu un displeja detaļas sadaļā Kontroles laukums <br> <br> <b> Taustiņi </b>: <br> <br> Bultas: pan <br> b, 'B' ird's-eye Skatīt / pagriezt par 'c' vai 'Esc': 'C'lear Data Selections <br>' d ':' D'ata Grid <br> 'h': 'H'ide / Re-display Izvēlne",
+		"datGuiFolders.selection.folders.data":					"Dati",
+		"datGuiFolders.dataGrid":					""
+		},//"lv"
+	"nb":		{
+		"appTitle":					"Data Visualizer",
+		"datGuiOriginalColor":					"Opprinnelig",
+		"dataGridTitle":					"Data",
+		"helpText3":					"<br> CtrlKey + Venstre: Velg et objekt og vis detaljer i kontrollområdet <br> <br> <b> Nøkler </ b>: <br> <br> Piler: Pan <br> 'b': 'B' ird's-eye View / Rotate <br> 'c' eller 'Esc': 'C'lear Data Selections <br>' d ':' D'ata Grid <br> 'h': 'H'ide / Re-display Meny",
+		"datGuiFolders.selection.folders.data":					"Data",
+		"datGuiFolders.dataGrid":					"Datamaskin"
+		},//"nb"
+	"fa":		{
+		"appTitle":					"ویژوالایزر داده",
+		"datGuiOriginalColor":					"اصلی",
+		"dataGridTitle":					"داده ها",
+		"helpText3":					"<br> CtrlKey + چپ: انتخاب یک شی و نمایش جزئیات در قسمت Controls <br> <br> <b> کلید </ b> <br> <br> <br> <br> <br> <br> <br> <br> 'b': 'B' 'd': 'D' ata Grid <br> 'h': 'H'ide / نمایش مجدد فهرست",
+		"datGuiFolders.selection.folders.data":					"داده ها",
+		"datGuiFolders.dataGrid":					"شبکه داده"
+		},//"fa"
+	"pt":		{
+		"appTitle":					"Visualizador de dados",
+		"datGuiOriginalColor":					"Original",
+		"dataGridTitle":					"Dados",
+		"helpText3":					"<br> CtrlKey + Esquerda: Selecione um objeto e exiba detalhes na área Controles <br> <br> <b> Chaves </ b>: <br> <br> Arrows: Pan <br> 'b': 'B' Ird's-View / Rotate <br> 'c' ou 'Esc': 'Seleções de dados de C'lear' d ':' Grid de D'A <br> 'h': 'H'ide / re-display Cardápio",
+		"datGuiFolders.selection.folders.data":					"Dados",
+		"datGuiFolders.dataGrid":					"Grade de dados"
+		},//"pt"
+	"ro":		{
+		"appTitle":					"Vizualizatorul de date",
+		"datGuiOriginalColor":					"Original",
+		"dataGridTitle":					"Date",
+		"helpText3":					"<br> CtrlKey + Stânga: Selectați un obiect și afișați detaliile în zona de control <br> <br> <b> Chei </ b>: <br> <br> Arrows: Pan <br> 'b': 'B' Vizualizarea / rotirea lui 'c' sau 'Esc': 'Selecții de date c'lear' d ':' D'ata Grid 'h': 'H'ide / Re-display Meniul",
+		"datGuiFolders.selection.folders.data":					"Date",
+		"datGuiFolders.dataGrid":					"Grila de date"
+		},//"ro"
+	"ru":		{
+		"appTitle":					"Визуализатор данных",
+		"datGuiOriginalColor":					"оригинал",
+		"dataGridTitle":					"Данные",
+		"helpText3":					"<br> CtrlKey + Влево: выберите объект и отобразите сведения в области «Управление». <br> <br> <b> Клавиши </ b>: <br> <br> Стрелки: «Панорамирование»: «b»: «B» Вид с высоты птичьего полета / Поворот <br> 'c' или 'Esc': 'Выборы данных C'lear <br>' d ':' Сетка D'ata <br> 'h': 'H'ide / Re-display Меню",
+		"datGuiFolders.selection.folders.data":					"Данные",
+		"datGuiFolders.dataGrid":					"Сетка данных"
+		},//"ru"
+	"sr":		{
+		"appTitle":					"Дата Висуализер",
+		"datGuiOriginalColor":					"Оригинал",
+		"dataGridTitle":					"Дата",
+		"helpText3":					"<бр> ЦтрлКеи + Лево: Изаберите објекат и прикажите детаље у области Контроле <бр> <бр> <б> Кључеви </б>: <бр> <бр> Стрелице: Пан <бр> 'б': 'Б' ирд'с-еие Виев / Ротате 'ц' или 'Есц': 'Ц'леар Дата Селецтионс' д ':' Д'ата Грид - 'х': 'Х'иде / Ре-дисплаи Мени",
+		"datGuiFolders.selection.folders.data":					"Дата",
+		"datGuiFolders.dataGrid":					"Дата Грид"
+		},//"sr"
+	"sk":		{
+		"appTitle":					"Data Visualizer",
+		"datGuiOriginalColor":					"originál",
+		"dataGridTitle":					"údaje",
+		"helpText3":					"<br> CtrlKey + Left: Vyberte objekt a zobrazte podrobnosti v oblasti Controls <br> <br> <b> Kľúče </b>: <br> <br> Šípky: Pan <br> 'b': 'B' ird's-eye Zobraziť / Otočiť <br> 'c' alebo 'Esc': 'C'lear Výber údajov <br>' d ':' D'ata Grid <br> 'h': 'H'ide / Re-display Ponuka",
+		"datGuiFolders.selection.folders.data":					"údaje",
+		"datGuiFolders.dataGrid":					"Dátová mriežka"
+		},//"sk"
+	"sl":		{
+		"appTitle":					"Prikazovalnik podatkov",
+		"datGuiOriginalColor":					"Izvirnik",
+		"dataGridTitle":					"Podatki",
+		"helpText3":					"<br> CtrlKey + Levo: Izberite predmet in podrobnosti prikaza na področju Kontrolniki <br> <br> <b> Ključi </b>: <br> <br> Puščice: Pan <br> 'b': 'B' ird's-eye Ogled / Zasuk <br> 'c' ali 'Esc': 'Izbira podatkov C'lear | Meni",
+		"datGuiFolders.selection.folders.data":					"Podatki",
+		"datGuiFolders.dataGrid":					"Podatkovna mreža"
+		},//"sl"
+} //translations
